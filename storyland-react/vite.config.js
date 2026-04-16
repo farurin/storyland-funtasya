43857e968d36d66bbac06ed7845877076d76a35d
farurin <luthfiyanama@gmail.com>
2026-04-16 15:05:39 +0700

    update vite config for cloudinary access

--- storyland-react/vite.config.js
+++ storyland-react/vite.config.js
@@ -6,4 +6,11 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
+
+  // Akses untuk Cloudinary:
+  define: {
+    "process.env.CLOUDINARY_URL": JSON.stringify(
+      "https://res.cloudinary.com/dsqxxvimn/image/upload/",
+    ),
+  },
 });
