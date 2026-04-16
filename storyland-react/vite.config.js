eb7aeeb71e5ef371d02ccd3cd2861c42918f7dfd
farurin <luthfiyanama@gmail.com>
2026-04-16 15:17:10 +0700

    fix vercel: remove flowbite plugin

--- storyland-react/vite.config.js
+++ storyland-react/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react(), tailwindcss()],
 
-  // Akses untuk Cloudinary:
   define: {
     "process.env.CLOUDINARY_URL": JSON.stringify(
       "https://res.cloudinary.com/dsqxxvimn/image/upload/",
