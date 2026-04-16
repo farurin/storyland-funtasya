f60cc4647be5ac40527f4aea2ffb4101d68ea53e
farurin <luthfiyanama@gmail.com>
2026-04-16 14:58:45 +0700

    prepare for deploy

--- storyland-api/config/db.js
+++ storyland-api/config/db.js
@@ -1,9 +1,15 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   waitForConnections: true,
   connectionLimit: 10, // Maksimal 10 koneksi bersamaan
   queueLimit: 0,
@@ -13,10 +19,10 @@ const db = mysql.createPool({
 const testConnection = async () => {
   try {
     const connection = await db.getConnection();
-    console.log("Berhasil terkoneksi ke pool database funtasya_storyland!");
+    console.log("Berhasil terkoneksi ke Database Cloud (Aiven)!");
     connection.release(); // Kembalikan koneksi ke kolam setelah dites
   } catch (err) {
-    console.error("Gagal terkoneksi ke MySQL Laragon:", err.message);
+    console.error("Gagal terkoneksi ke Database Aiven:", err.message);
   }
 };
 
